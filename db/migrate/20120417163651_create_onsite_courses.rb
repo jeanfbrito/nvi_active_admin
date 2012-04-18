5dde984fdc8c2edac1903da5f20bb9da2134f5ea
Will Cosgrove <will@willcosgrove.com>
2012-04-17 23:05:18 -0500

    Added back aa

--- db/migrate/20120417163651_create_onsite_courses.rb
+++ db/migrate/20120417163651_create_onsite_courses.rb
@@ -3,7 +3,7 @@ class CreateOnsiteCourses < ActiveRecord::Migration
     create_table :onsite_courses do |t|
       t.string :title
       t.text :description
-      t.integer :price_in_pennies
+      t.integer :price_in_pennies, default: 0
       t.integer :down_payment_price_in_pennies, default: 200000
       t.integer :position
       t.integer :market_sector_id
--- db/migrate/20120417165433_create_online_lessons.rb
+++ db/migrate/20120417165433_create_online_lessons.rb
@@ -3,7 +3,7 @@ class CreateOnlineLessons < ActiveRecord::Migration
     create_table :online_lessons do |t|
       t.string :title
       t.text :description
-      t.integer :price_in_pennies
+      t.integer :price_in_pennies, default: 0
       t.string :video_uid
       t.integer :online_course_id
 
