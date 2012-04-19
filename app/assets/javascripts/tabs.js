51080e6cbcee0da9b15c636d9671b3c19a344fec
Will Cosgrove <will@willcosgrove.com>
2012-04-19 14:32:25 -0500

    Added bottom slider

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,6 +1,10 @@
 // JavaScript Document
 
 $(document).ready(function() {
+  $(".slider1").jCarouselLite({
+    btnNext: ".leftarrow",
+    btnPrev: ".rightarrow"
+  });
   $(".tab_content").hide(); //Hide all content
   $(".tabs_links ul li:first").addClass("active").show(); //Activate first tab
   $(".tab_content:first").show(); //Show first tab content
@@ -30,10 +34,6 @@ $(document).ready(function() {
 });
 
 $(function() {
-    $(".slider1").jCarouselLite({
-        btnNext: ".leftarrow",
-        btnPrev: ".rightarrow"
-    });
 	$("a.galleryimg").fancybox({
 'titlePosition'		: 'inside',
 'overlayColor'		: '#000',
