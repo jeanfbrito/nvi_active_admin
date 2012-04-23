3040b964dfc5d43791fc3b213204d7e69f714549
Will Cosgrove <will@willcosgrove.com>
2012-04-23 01:37:50 -0500

    Updated caching on unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,4 +8,9 @@ end
 
 after_fork do |server, worker|
   ActiveRecord::Base.establish_connection
+  Rails.cache.reset if Rails.cache.respond_to?(:reset)
+
+  # Reset Rails's session store
+  # If you know a cleaner way to find the session store instance, please let me know
+  ObjectSpace.each_object(ActionDispatch::Session::DalliStore) { |obj| obj.reset }
 end
